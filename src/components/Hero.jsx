8d598df18d21c1b937a3d6777d711864c2f0e7e7
Emma Richmond <richmond.emma.g@gmail.com>
2025-07-02 17:44:57 -0400

    added simple hero section to front page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hero =  () => {
+    return (
+        <div className="hero-section">
+            <h2>Emma Richmond</h2>
+            <h3>Software Engineer</h3>
+            <p>Hi, I'm Emma Richmond, a software engineer with a passion
+                for building responsive web application and solving
+                solving real-world problems through clean, efficient code.
+            </p>
+            <button><p>View My Projects</p></button>
+        </div>
+
+    );
+
+};
+
+export default Hero;
